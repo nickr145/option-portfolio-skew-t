f1dd7e7d3453b52afa8c7d83b7e867ad5b401acf
NR <nicholas.rebello@gmail.com>
2026-06-23 15:38:47 -0400

    completed option_portfolio initializations and distribution code

---
+++ option_portfolio/__init__.py
@@ -0,0 +1,25 @@
+"""
+Option Portfolio Optimization under Skew-Elliptical t Returns
+
+Implementation of Sung & Pirvu (2026) "Sharpe Ratio and Return-VaR Ratio 
+Maximization for Option Portfolios with Skew-Elliptical t Underlying Returns"
+
+Main classes:
+    - OptionPortfolioOptimizer: End-to-end optimization pipeline
+    - SkewTFitter: Fit multivariate skew-t distribution
+    - BlackScholesGreeks: Compute option Greeks
+    - DeltaGammaPortfolio: Assemble delta-gamma parameters
+"""
+
+from .optimizer import OptionPortfolioOptimizer
+from .distributions import SkewTFitter
+from .greeks import BlackScholesGreeks, DeltaGammaPortfolio
+
+__version__ = "1.0.0"
+__author__ = "Nicholas Rebello"
+__all__ = [
+    "OptionPortfolioOptimizer",
+    "SkewTFitter",
+    "BlackScholesGreeks",
+    "DeltaGammaPortfolio"
+]
